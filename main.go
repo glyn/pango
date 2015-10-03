daa25bfee3bdf438ea6210546d18887d4e25867a
Glyn Normington <gnormington@gopivotal.com>
2015-10-04 00:02:56 +0100

    Basic module dependency metrics
    See, for example, the following article:
    http://www.drdobbs.com/the-tipping-point-stability-and-instabil/184415285

--- main.go
+++ main.go
@@ -33,6 +33,8 @@ func main() {
 func discover(c *cli.Context) {
 	d := disc.New(c.Args().First(), c.Args().Get(1))
 	imports := d.Discover()
+
+	// Analyse self-contained packages.
 	for p, _ := range imports {
 		escape := false
 		d.Walk(p, func(q string, qi []string) {
@@ -44,6 +46,39 @@ func discover(c *cli.Context) {
 		})
 		if !escape {
 			fmt.Printf("%s is self-contained\n", d.ShortName(p))
+		}
+	}
+
+	// Compute scoped fan-out of each package.
+	fanOut := make(map[string]int, 1)
+	for p, imp := range imports {
+		fo := len(imp)
+		fanOut[p] = fo
+		fmt.Printf("%s has fan-out %d\n", p, fo)
+	}
+
+	// Compute scoped fan-in of each package.
+	fanIn := make(map[string]int, 1)
+	for _, imp := range imports {
+		for _, i := range imp {
+			fanIn[i] = fanIn[i] + 1
+		}
+	}
+
+	// Compute scoped instability of each package.
+	instab := make(map[string]float32, 1)
+	for p, _ := range imports {
+		instability := float32(fanOut[p]) / float32((fanIn[p] + fanOut[p]))
+		instab[p] = instability
+		fmt.Printf("%s has fan-in %d and instability %.2f\n", p, fanIn[p], instability)
+	}
+
+	// Check stable dependencies principle violations
+	for p, imp := range imports {
+		for _, q := range imp {
+			if instab[q] > instab[p] {
+				fmt.Printf("%s depends on %s but the former is more stable than the latter\n", p, q)
+			}
 		}
 	}
 }
